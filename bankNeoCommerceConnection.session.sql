2deef8757dea0c4258b1cf6df94c075701ee5077
Christopher <christopher@Christophers-MacBook-Pro.local>
2024-05-28 10:12:29 +0700

    Add zipped project file

--- bankNeoCommerceConnection.session.sql
+++ bankNeoCommerceConnection.session.sql
@@ -1,35 +1,35 @@
-CREATE TABLE User (
-    CorporateAccount varchar(150),
-    CorporateName varchar(150),
-    UserId varchar(50),
-    UserName varchar(50),
-    UserRole varchar(30),
-    PhoneNumber varchar(30),
-    Email varchar(100),
-    Verification varchar(100),
-    Password varchar(100)
-);
+-- CREATE TABLE User (
+--     CorporateAccount varchar(150),
+--     CorporateName varchar(150),
+--     UserId varchar(50),
+--     UserName varchar(50),
+--     UserRole varchar(30),
+--     PhoneNumber varchar(30),
+--     Email varchar(100),
+--     Verification varchar(100),
+--     Password varchar(100)
+-- );
 
 
-CREATE TABLE TransactionList (
-    ReferenceNo varchar(100),
-    TransferAmount varchar(100),
-    TransferRecord varchar(100),
-    FromAccountNo varchar(100),
-    ToAccountNo varchar(100),
-    ToAccountName varchar(100),
-    ToAccountBank varchar(100),
-    Description varchar(255),
-    Maker varchar(100),
-    TransferDate TIMESTAMP,
-    ApprovalStatus varchar(100)
-)
+-- CREATE TABLE TransactionList (
+--     ReferenceNo varchar(100),
+--     TransferAmount varchar(100),
+--     TransferRecord varchar(100),
+--     FromAccountNo varchar(100),
+--     ToAccountNo varchar(100),
+--     ToAccountName varchar(100),
+--     ToAccountBank varchar(100),
+--     Description varchar(255),
+--     Maker varchar(100),
+--     TransferDate TIMESTAMP,
+--     ApprovalStatus varchar(100)
+-- )
 
-CREATE TABLE TransactionOverview (
-    AwaitingApproval int,
-    Approved int,
-    Rejected int,
-)
+-- CREATE TABLE TransactionOverview (
+--     AwaitingApproval int,
+--     Approved int,
+--     Rejected int
+-- )
 
 
 
